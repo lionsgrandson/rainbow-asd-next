3f6875c8cb729195c8c87885ca4d009f7ef89d65
lionsgrandson <mosheschwartzberg@gmail.com>
2025-09-28 15:55:51 +0300

    feat: changed to the meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,28 @@
 import '../styles/globals.css'
 import '../styles/App.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        id='clarity-script'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+      console.log('🔍 Attempting to load Clarity...');
+      (function(c,l,a,r,i,t,y){
+        console.log('🔍 Inside Clarity init');
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r); t.async=1; t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "rnf7qn6idb");
+    `,
+        }}
+      />
+
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,61 @@ import RecSec from '../components/RecSec'
 import ContactBtn from '../components/ContactBtn'
 import Footer from '../components/Footer'
 import styles from '../styles/index.module.css'
+import Head from 'next/head'
 export default function Home() {
   return (
     <main className={styles.FirstBox}>
+      <Head>
+        {/* Primary Meta Tags */}
+        {/* <!-- Primary Meta Tags --> */}
+        <title>
+          הנחייה אישית - לשילוב יחידני ולמתבגרים ובוגרים על הרצף האוטיסטי
+        </title>
+        <meta
+          name='title'
+          content='הנחייה אישית - לשילוב יחידני ולמתבגרים ובוגרים על הרצף האוטיסטי'
+        />
+        <meta
+          name='description'
+          content='אני כאן בשבילכם – ללוות, להדריך ולבנות יחד מסלול מותאם אישית לילד שלכם, צעד אחר צעד. ליווי אישי למתבגרים ובוגרים עם אוטיזם, סדנאות להורים, וייעוץ מקצועי מבוסס ניסיון אישי, קליני ואקדמי.'
+        />
+        {/* 
+<!-- Open Graph / Facebook --> */}
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://rainbow-asd.com/' />
+        <meta
+          property='og:title'
+          content='הנחייה אישית - לשילוב יחידני ולמתבגרים ובוגרים על הרצף האוטיסטי'
+        />
+        <meta
+          property='og:description'
+          content='אני כאן בשבילכם – ללוות, להדריך ולבנות יחד מסלול מותאם אישית לילד שלכם, צעד אחר צעד. ליווי אישי למתבגרים ובוגרים עם אוטיזם, סדנאות להורים, וייעוץ מקצועי מבוסס ניסיון אישי, קליני ואקדמי.'
+        />
+        <meta
+          property='og:image'
+          content='https://rainbow-asd.com/_next/static/media/Logo_icon_noshadow.408216b7.png'
+        />
+
+        {/* <!-- X (Twitter) --> */}
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta
+          property='twitter:url'
+          content='https://rainbow-asd.com/_next/static/media/Logo_icon_noshadow.408216b7.png'
+        />
+        <meta
+          property='twitter:title'
+          content='הנחייה אישית - לשילוב יחידני ולמתבגרים ובוגרים על הרצף האוטיסטי'
+        />
+        <meta
+          property='twitter:description'
+          content='אני כאן בשבילכם – ללוות, להדריך ולבנות יחד מסלול מותאם אישית לילד שלכם, צעד אחר צעד. ליווי אישי למתבגרים ובוגרים עם אוטיזם, סדנאות להורים, וייעוץ מקצועי מבוסס ניסיון אישי, קליני ואקדמי.'
+        />
+        <meta
+          property='twitter:image'
+          content='https://rainbow-asd.com/_next/static/media/Logo_icon_noshadow.408216b7.png'
+        />
+      </Head>
+
       <Hero />
       <ContactSec />
       <QnaSec />
