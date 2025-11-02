2786cc98b77e07962c8de83380c35143312c11d7
lionsgrandson <mosheschwartzberg@gmail.com>
2025-11-02 09:51:31 +0200

    style: footer image is now text + image.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,24 @@ import { FaPhoneAlt } from 'react-icons/fa'
 import { MdEmail } from 'react-icons/md'
 import Image from 'next/image'
 import Link from 'next/link'
-import logoLongTxt from '../../public/img/Logo_with_NO_shadow_text.png'
+// import logoLongTxt from '../../public/img/Logo_with_NO_shadow_text.png'
+import logoLongTxt from '../../public/img/Logo_icon_noshadow.png'
 import styles from '../styles/footer.module.css'
 
 export default function Footer() {
   return (
     <>
       <div className={styles.footerSec}>
-        <Image
-          src={logoLongTxt}
-          className={styles.footerImg}
-          alt='לוגו עם סיסמה ופאזל בצבעי הקשת'
-        />
+        <div className={styles.footerSecimg}>
+          <Image
+            src={logoLongTxt}
+            className={styles.footerImg}
+            alt='לוגו עם סיסמה ופאזל בצבעי הקשת'
+          />
+          <p className={styles.footerSecimgp}>
+            הנחייה אישית להורים וילדים עם אוטיזם
+          </p>
+        </div>
         <div className={styles.ContactFooter}>
           <div>
             <a href='tel:+972546495902'>054-649-5902</a>
